653ce970ba7f2f1c4ec9e5ca84ab8308d35b428f
atsushieno <atsushieno@gmail.com>
2020-05-06 04:00:36 +0900

    finished unverified LV2UI support code. Next: need to integrate with sfizz...

--- aria2web-lv2ui.c
+++ aria2web-lv2ui.c
@@ -1,5 +1,8 @@
 #include <lv2/lv2plug.in/ns/lv2core/lv2.h>
 #include <lv2/lv2plug.in/ns/extensions/ui/ui.h>
+#include <lv2/lv2plug.in/ns/ext/atom/atom.h>
+#include <lv2/lv2plug.in/ns/ext/midi/midi.h>
+#include <lv2/lv2plug.in/ns/ext/urid/urid.h>
 #define HTTPSERVER_IMPL
 #define ARIA2WEB_IMPL
 #include "aria2web.h"
@@ -18,9 +21,25 @@ typedef struct aria2weblv2ui_tag {
 	LV2UI_Write_Function write_function;
 	LV2UI_Controller controller;
 	LV2UI_Widget *widget;
+	const LV2_Feature *const *features;
+	int urid_atom, urid_beat_time, urid_midi_event;
 	
-	float buf[2];
+	char atom_buffer[50];
 } aria2weblv2ui;
+
+char* fill_atom_message_base(aria2weblv2ui* a, LV2_Atom_Sequence* seq)
+{
+	auto events = (LV2_Atom_Event*) ((char*) seq + sizeof (LV2_Atom_Sequence));
+	seq->body.unit = a->urid_beat_time;
+	auto ev = &events[0];
+	auto msg = ((char*) ev + sizeof (LV2_Atom_Event));
+	ev->time.frames = 0;
+	ev->body.type = a->urid_midi_event;
+	ev->body.size = 3;
+	seq->atom.size = sizeof(LV2_Atom_Event) + 3;
+	seq->atom.type = a->urid_atom;
+	return msg;
+}
 
 void a2wlv2_cc_callback(void* context, int cc, int value)
 {
@@ -28,10 +47,13 @@ void a2wlv2_cc_callback(void* context, int cc, int value)
 
 	puts("a2wlv2_cc_callback invoked");
 
-	// TODO: generate Atom MIDI message here.
-	a->buf[0] = (float) value;
+	auto seq = (LV2_Atom_Sequence*) a->atom_buffer;
+	char* msg = fill_atom_message_base(a, seq);
+	msg[0] = 0xB0;
+	msg[1] = (char) cc;
+	msg[2] = (char) value;
 	
-	a->write_function(a->controller, ARIA2WEB_LV2_CONTROL_PORT, 1, LV2_UI__floatProtocol, &a->buf);
+	a->write_function(a->controller, ARIA2WEB_LV2_CONTROL_PORT, seq->atom.size + sizeof(LV2_Atom), LV2_UI__floatProtocol, a->atom_buffer);
 }
 
 void a2wlv2_note_callback(void* context, int key, int velocity)
@@ -40,11 +62,14 @@ void a2wlv2_note_callback(void* context, int key, int velocity)
 
 	puts("a2wlv2_note_callback invoked");
 	
-	// TODO: generate Atom MIDI message here.
-	a->buf[0] = (float) key;
-	a->buf[1] = (float) velocity;
 
-	a->write_function(a->controller, ARIA2WEB_LV2_CONTROL_PORT, 2, LV2_UI__floatProtocol, &a->buf);
+	auto seq = (LV2_Atom_Sequence*) a->atom_buffer;
+	char* msg = fill_atom_message_base(a, seq);
+	msg[0] = velocity == 0 ? 0x80 : 0x90;
+	msg[1] = (char) key;
+	msg[2] = (char) velocity;
+	
+	a->write_function(a->controller, ARIA2WEB_LV2_CONTROL_PORT, seq->atom.size + sizeof(LV2_Atom), LV2_UI__floatProtocol, a->atom_buffer);
 }
 
 LV2UI_Handle aria2web_lv2ui_instantiate(
@@ -60,6 +85,22 @@ LV2UI_Handle aria2web_lv2ui_instantiate(
 	ret->a2w = aria2web_create();
 	aria2web_set_control_change_callback(ret->a2w, a2wlv2_cc_callback, ret);
 	aria2web_set_note_callback(ret->a2w, a2wlv2_note_callback, ret);
+	ret->plugin_uri = plugin_uri;
+	ret->bundle_path = bundle_path;
+	ret->write_function = write_function;
+	ret->controller = controller;
+	ret->widget = widget;
+	ret->features = features;
+
+	for (auto f : std::vector<const LV2_Feature*>(features)) {
+		if (strcmp(f.URI, LV2_ATOM_URI) == 0) {
+			auto urid = (LV2_URID_Map*) f.data;
+			ret->urid_atom = urid->map(urid->handle, LV2_ATOM_URI);
+			ret->urid_beat_time = urid->map(urid->handle, LV2_ATOM__beatTime);
+			ret->urid_midi_event = urid->map(urid->handle, LV2_MIDI__MidiEvent);
+			break;
+		}
+	}
 }
 
 void aria2web_lv2ui_cleanup(LV2UI_Handle ui)
