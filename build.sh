8e8b6d7db61571982d60561b57b9f6f146ee5ab2
Atsushi Eno <atsushieno@gmail.com>
2020-05-12 01:45:40 +0900

    slightly improved build instruction.

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 g++ -g -fpermissive -I external/webview/ -I external/httpserver.h/ aria2web-host.c `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o aria2web-host  || exit 1
 g++ -g -fpermissive -I external/webview/ -I external/httpserver.h/ aria2web-lv2ui.c `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o aria2web-lv2ui.so -shared -fPIC -Wl,--no-undefined || exit 1
 
-if [ ! -d sfizz-aria2web ] ; then
+if [ ! -d sfizz-aria2web/dist ] ; then
   echo "building sfizz..." ;
   mkdir sfizz-aria2web ;
   cd sfizz-aria2web ;
   cmake -DCMAKE_INSTALL_PREFIX=`pwd`/dist ../external/sfizz ;
+  make ;
   make install ;
   cd .. ;
 fi
